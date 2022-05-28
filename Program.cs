2230aee83ba670fd348d1557d2286e8d1547b684
Ivan Malkovskii <103892616+gotikym@users.noreply.github.com>
2022-05-28 20:08:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 internal class Program
 {
     static void Main(string[] args)
@@ -16,15 +16,21 @@ internal class Program
                     sum += array[i, j];
                 }
 
-                if (j == 0)
-                {
-                    product *= array[i, j];
-                }
-
                 Console.Write(array[i, j] + " ");
             }
 
             Console.WriteLine();
+        }
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (j == 0)
+                {
+                    product *= array[i, j];
+                }
+            }
         }
 
         Console.WriteLine("Произведение первого столбца равно - " + product);
